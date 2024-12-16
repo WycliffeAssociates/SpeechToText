8a4b0f02b756710c161480d9ccad7173021e2a8a
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2024-12-16 15:14:11 -0600

    some changes for deploy

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -z "$DEPLOY_ENV" ]; then
-  echo "Error: Please set the 'DEPLOY_ENV' environment variable."
-  exit 1
-fi
+# if [ -z "$DEPLOY_ENV" ]; then
+#   echo "Error: Please set the 'DEPLOY_ENV' environment variable."
+#   exit 1
+# fi
 
 
 if [ -z "$OP_SERVICE_ACCOUNT_TOKEN" ]; then
@@ -18,10 +18,6 @@ set -x
 alias op="docker run --rm -e OP_SERVICE_ACCOUNT_TOKEN 1password/op:2 op"
 
 export OP_SERVICE_ACCOUNT_TOKEN=$OP_SERVICE_ACCOUNT_TOKEN
-
-# Define environment variables
-export DNS_NAME_APP=${DNS_NAME_APP}
-export DNS_NAME_SERVER=${DNS_NAME_SERVER}
 
 # Build and start the Docker containers
 docker compose down
