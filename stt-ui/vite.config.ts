de4ff69d05f31fca7a8185f9a84fca6e4482277a
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2024-12-19 14:54:08 -0600

    expose vite port

--- stt-ui/vite.config.ts
+++ stt-ui/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5001,
+  },
+});
